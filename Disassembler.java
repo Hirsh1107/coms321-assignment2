281dcfe87041b4489e6a82f509f18ca868181b1e
Drew Hirsh <ahirsh@iastate.edu>
2023-12-04 15:40:03 -0600

    int arr to decimal

--- Disassembler.java
+++ Disassembler.java
@@ -6,4 +6,15 @@ public class Disassembler {
         }
         String fileName = args[0];
     }
+
+    //idk if this works yet
+    private int binaryToDecimal(int[] binaryArr) {
+        int result = 0;
+        for(int i = 0; i = binaryArr.length; i++) {
+            if(binaryArr[i] == 1) {
+                result += Math.pow(2, binaryArr.length - i - 1);
+            }
+        }
+        return result;
+    }
 }
