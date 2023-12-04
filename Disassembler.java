fa65eb4ca0b463dc7beff73f3235e020f114d379
ahirsh <ahirsh@pyrite-n3.cs.iastate.edu>
2023-12-04 17:43:38 -0600

    you can run the program :D

--- Disassembler.java
+++ Disassembler.java
@@ -4,13 +4,14 @@ public class Disassembler {
         if (args.length != 1) {
             System.exit(1);
         }
-        String fileName = args[0];
+        String fileName = args[0];   
     }
 
+    
     //idk if this works yet
     private int binaryToDecimal(int[] binaryArr) {
         int result = 0;
-        for(int i = 0; i <= binaryArr.length; i++) {
+        for(int i = 0; i < binaryArr.length; i++) {
             if(binaryArr[i] == 1) {
                 result += Math.pow(2, binaryArr.length - i - 1);
             }
