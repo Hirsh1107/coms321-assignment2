3273c8c8b947405be6aefaf36819be86b7481999
Hirsh1107 <46503729+Hirsh1107@users.noreply.github.com>
2023-12-03 19:36:30 -0600

    Create Disassembler.java

---
+++ Disassembler.java
@@ -0,0 +1,9 @@
+public class Disassembler {
+  
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.exit(1);
+        }
+        String fileName = args[0];
+    }
+}
