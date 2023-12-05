75d64f720ee8e7affa00ae0a8f1c59d8c3f8d871
timkuehn <timkuehn@iastate.edu>
2023-12-04 18:18:15 -0600

    push for drew to make branch

--- Disassembler.java
+++ Disassembler.java
@@ -1,15 +1,20 @@
 public class Disassembler {
   
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.exit(1);
-        }
-        String fileName = args[0];   
+       int[] binary = {1,0,0,0,1,0,1,1,0,0,0};
+       int decimal = binaryToDecimal(binary);
+       System.out.println(decimal);
+       System.out.print(new OpCode(decimal));
+
+        // if (args.length != 1) {
+        //     System.exit(1);
+        // }
+        // String fileName = args[0];   
     }
 
     
     //idk if this works yet
-    private int binaryToDecimal(int[] binaryArr) {
+    private static int binaryToDecimal(int[] binaryArr) {
         int result = 0;
         for(int i = 0; i < binaryArr.length; i++) {
             if(binaryArr[i] == 1) {
--- OpCode.java
+++ OpCode.java
@@ -5,7 +5,7 @@ public class OpCode {
     private int opCodeValue;
     private int opWidth;
     private String name;
-    private format format; 
+    private format opFormat; 
     private int shamt;
 
     enum format {
@@ -17,203 +17,273 @@ public class OpCode {
         IM
       }
 
+
     public OpCode(){
     }
+
+    public int getOpCodeValue() {
+        return opCodeValue;
+    }
     
+    public void setOpCodeValue(int opCodeValue) {
+        this.opCodeValue = opCodeValue;
+    }
+    
+    public int getOpWidth() {
+        return opWidth;
+    }
+    
+    public void setOpWidth(int opWidth) {
+        this.opWidth = opWidth;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public format getOpFormat() {
+        return opFormat;
+    }
+    
+    public void setOpFormat(format opFormat) {
+        this.opFormat = opFormat;
+    }
+    
+    public int getShamt() {
+        return shamt;
+    }
+    
+    public void setShamt(int shamt) {
+        this.shamt = shamt;
+    }
+    
+
+    @Override
+    public String toString() {
+    return "OpCode{" +
+            "opCodeValue=" + opCodeValue +
+            ", opWidth=" + opWidth +
+            ", name='" + name + '\'' +
+            ", opFormat=" + opFormat +
+            ", shamt=" + shamt +
+            '}';
+    }
+
     public OpCode(int opCode){
+        if(opCode==581){
+            opCode = 580;
+        }
+        else if(opCode==585){
+            opCode = 584;
+        }
+        else if(opCode>=160 && opCode<=191){
+            opCode = 5;
+        }
+        else if(opCode>=672 && opCode<=679){
+            opCode = 84;
+        }
+        else if(opCode>=1184 && opCode<)
+        
         switch (opCode){
         
             case 1112: 
                 opCodeValue = 1112;
                 opWidth = 11;
                 name = "ADD";
-                format = format.R;
+                opFormat = format.R;
                 break;
 
             case 580:
                 opCodeValue = 580;
                 opWidth = 10;
                 name = "ADDI";
-                format = format.I;
+                opFormat = format.I;
                 break;
 
            case 1104:
                 opCodeValue = 1104;
                 opWidth = 11;
                 name = "AND";
-                format = format.R;
+                opFormat = format.R;
                 break;
 
             case 5:
                 opCodeValue = 5;
                 opWidth = 6;
                 name = "B";
-                format = format.B;
+                opFormat = format.B;
                 break;
 
             case 84:
                 opCodeValue = 84;
                 opWidth = 8;
                 name = "B.cond";
-                format = format.CB;
+                opFormat = format.CB;
                 break;
 
             case 37:
                 opCodeValue = 37;
                 opWidth = 6;
                 name = "BL";
-                format = format.B;
+                opFormat = format.B;
                 break;
 
             case 1712:
                 opCodeValue = 1712;
                 opWidth = 11;
                 name = "BR";
-                format = format.R;
+                opFormat = format.R;
                 break;
 
             case 181:
                 opCodeValue = 181;
                 opWidth = 8;
                 name = "CBNZ";
-                format = format.CB;
+                opFormat = format.CB;
                 break;
 
             case 180:
                 opCodeValue = 180;
                 opWidth = 8;
                 name = "CBZ";
-                format = format.CB;
+                opFormat = format.CB;
                 break;
             
             case 1616:
                 opCodeValue = 1616;
                 opWidth = 11;
                 name = "EOR";
-                format = format.R;
+                opFormat = format.R;
                 break;
 
             case 840:
                 opCodeValue = 840;
                 opWidth = 10;
                 name = "EORI";
-                format = format.I;
+                opFormat = format.I;
                 break;
             
             case 1986:
                 opCodeValue = 1986;
                 opWidth = 11;
                 name = "LDUR";
-                format = format.D;
+                opFormat = format.D;
                 break;
 
             case 1691:
                 opCodeValue = 1691;
                 opWidth = 11;
                 name = "LSL";
-                format = format.R;
+                opFormat = format.R;
                 break;
             
             case 1690:
                 opCodeValue = 1690;
                 opWidth = 11;
                 name = "LSR";
-                format = format.R;
+                opFormat = format.R;
                 break;
 
             case 1360:
                 opCodeValue = 1360;
                 opWidth = 11;
                 name = "ORR";
-                format = format.R;
+                opFormat = format.R;
                 break;
                 
             case 712:
                 opCodeValue = 712;
                 opWidth = 10;
                 name = "ORRI";
-                format = format.I;
+                opFormat = format.I;
                 break;
             
             case 1984:
                 opCodeValue = 1984;
                 opWidth = 11;
                 name = "STUR";
-                format = format.D;
+                opFormat = format.D;
                 break;
             
             case 1624:
                 opCodeValue = 1624;
                 opWidth = 11;
                 name = "SUB";
-                format = format.R;
+                opFormat = format.R;
                 break;
             
             case 836:
                 opCodeValue = 836;
                 opWidth = 10;
                 name = "SUBI";
-                format = format.I;
+                opFormat = format.I;
                 break;
 
             case 964:
                 opCodeValue = 964;
                 opWidth = 10;
                 name = "SUBIS";
-                format = format.I;
+                opFormat = format.I;
                 break;
             
             case 1880:
                 opCodeValue = 1880;
                 opWidth = 11;
                 name = "SUBS";
-                format = format.R;
+                opFormat = format.R;
                 break;
             
             case 1240:
                 opCodeValue = 1240;
                 opWidth = 11;
                 name = "MUL";
-                format = format.R;
+                opFormat = format.R;
+                shamt = 31;
                 break;
             
             case 2045:
                 opCodeValue = 2045;
                 opWidth = 11;
                 name = "PRNT";
-                format = format.R;
+                opFormat = format.R;
                 break;
             
             case 2044:
                 opCodeValue = 2044;
                 opWidth = 11;
                 name = "PRNL";
-                format = format.R;
+                opFormat = format.R;
                 break;
 
             case 2046:
                 opCodeValue = 2046;
                 opWidth = 11;
                 name = "DUMP";
-                format = format.R;
+                opFormat = format.R;
                 break;
 
             case 2047:
                 opCodeValue = 2047;
                 opWidth = 11;
                 name = "HALT";
-                format = format.R;
+                opFormat = format.R;
                 break;
 
             default:
+            
                 opCodeValue = -1;
                 opWidth = -1;
                 name = "oopsieWoopsies uWU";
-                format = null;
+                opFormat = null;
                 break;
         }
 
         }
+
 
 
 }
