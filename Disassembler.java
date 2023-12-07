2aed385494230e1bd2847f888b1808514fe14204
timkuehn <timkuehn@iastate.edu>
2023-12-07 15:00:04 -0600

    updated to new style

--- Disassembler.java
+++ Disassembler.java
@@ -1,94 +1,243 @@
-import java.util.Arrays;
-
-import javax.management.RuntimeErrorException;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class Disassembler {
-  
+
     public static void main(String[] args) {
-       int[] binary = {1,1,0,1,0,0,1,1,0,1,1,   0,1,1,1,0,  0,0,0,0,0,0,    0,0,0,1,0, 0,0,0,0,1};
-       System.out.println(binary.length);
-       printArray(binary);
-       System.out.println();
-       OpCode test = findOpCode(binary);
-       rType(test, binary);
-    }
+        if (args.length != 1) {
+            System.out.println("Usage: java Disassembler <input_file>");
+            System.exit(1);
+        }
 
+        String inputFileName = args[0];
 
-    private static OpCode findOpCode(int[] binary){
-       int[] firstEleven = Arrays.copyOfRange(binary,0, 11);
-       int decimal = binaryToDecimal(firstEleven);
-       OpCode returnCode = new OpCode(decimal);
-       return returnCode;
-    }
-
-
-
-    
-    public static void printArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i]);
-            if (i != array.length - 1) {
-                System.out.print(",");
+        try (DataInputStream dis = new DataInputStream(new FileInputStream(inputFileName))) {
+            while (dis.available() >= 4) {
+                int instruction = dis.readInt();
+                disassemble(instruction);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-    
 
-    
-    private static int binaryToDecimal(int[] binaryArr) {
-        int result = 0;
-        for(int i = 0; i < binaryArr.length; i++) {
-            if(binaryArr[i] == 1) {
-                result += Math.pow(2, binaryArr.length - i - 1);
-            }
+    private static void disassemble(int instruction) {
+        int opcode = instruction >>> 21;
+        int offset = 0;
+        boolean instructionDefined = false;
+        int Rd;
+        int Rn;
+        int Rm;
+        int shamt;
+        int imm;
+        int addr;
+        int Rt;
+
+        //check for r-type and d-type
+        Rd = instruction & 0x1F;
+        Rn = (instruction >>> 5) & 0x1F;
+        Rm = (instruction >>> 16) & 0x1F;
+        shamt = (instruction >>> 10) & 0x3F;
+        String rd = correctReg(Rd);
+        String rn = correctReg(Rn);
+        String rm = correctReg(Rm);
+        switch (opcode) {
+            case 0b10001011000: // ADD
+                System.out.println("ADD " + rd + ", " + rn + ", " + rm);
+                instructionDefined = true;
+                break;
+
+            case 0b10001010000: // AND
+                System.out.println("AND " + rd + ", " + rn + ", " + rm);
+                instructionDefined = true;
+                break;
+
+            case 0b11010110000: // BR
+                System.out.println("BR " + rn);
+                instructionDefined = true;
+                break;
+            
+            case 0b11001010000: // EOR
+                System.out.println("EOR " + rd + ", " + rn + ", " + rm);
+                instructionDefined = true;
+                break;
+
+            case 0b11010011011: // LSL
+                System.out.println("LSL " + rd + ", " + rn + " #" + shamt);
+                instructionDefined = true;
+                break;
+
+            case 0b11010011010: // LSR
+                System.out.println("LSR " + rd + ", " + rn + " #" + shamt);
+                instructionDefined = true;
+                break;
+
+            case 0b10101010000: // ORR
+                System.out.println("OOR " + rd + ", " + rn + ", " + rm);
+                instructionDefined = true;
+                break;
+
+            case 0b11001011000: // SUB
+                System.out.println("SUB " + rd + ", " + rn + ", " + rm);
+                instructionDefined = true;
+                break;
+
+            case 0b11101011000: // SUBS
+                System.out.println("SUBS " + rd + ", " + rn + ", " + rm);
+                instructionDefined = true;
+                break;
+
+            case 0b10011011000: // MUL
+                System.out.println("OOR " + rd + ", " + rn + ", " + rm);
+                instructionDefined = true;
+                break;
+
+            case 0b11111111101: // PRNT
+                System.out.println("PRNT " + rm);
+                instructionDefined = true;
+                break;
+
+            case 0b11111111100: // PRNL
+                System.out.println("PRNL");
+                instructionDefined = true;
+                break;
+
+            case 0b11111111110: // DUMP
+                System.out.println("DUMP");
+                instructionDefined = true;
+                break;
+
+            case 0b11111111111: // HALT
+                System.out.println("HALT");
+                instructionDefined = true;
+                break;
+
+            //d-type
+            //this is not implemented yet, will probably have to do a seperate switch case
+            //to calculate the the correct registers and what not
+            case 0b11111000010: // LDUR
+                System.out.println("LDUR " + rd + ", [" + rn + ", #" + shamt + "]");
+                instructionDefined = true;
+                break;
+
+            //d-type
+            case 0b11111000000: // STUR
+                System.out.println("STUR " + rd + ", [" + rn + ", #" + shamt + "]");
+                instructionDefined = true;
+                break;
+
+            default:
+                break;
         }
-        return result;
-    }
+        if(instructionDefined) {
+			return;
+		}
+        //i-type
+        opcode = instruction >>> 22;
+        Rd = instruction & 0x1F;
+        Rn = (instruction >>> 5) & 0x1F;
+        imm = (instruction >>> 10) & 0xFFF;
+        rd = correctReg(Rd);
+        rn = correctReg(Rn);
+        switch (opcode) {
+            case 0b1001000100: // ADDI
+                System.out.println("ADDI " + rd + ", " + rn + ",  #" + imm);
+                instructionDefined = true;
+                break;
 
-    private static int[] decimalToBinary(int decimalNum) {
-       
-        int numBits = (int) (Math.log(decimalNum) / Math.log(2)) + 1;
-        int[] binaryArr = new int[numBits];
-        for (int i = numBits - 1; i >= 0; i--) {
-            binaryArr[i] = decimalNum % 2;
-            decimalNum /= 2;
+            case 0b1001001000: // ANDI
+                System.out.println("ANDI " + rd + ", " + rn + ",  #" + imm);
+                instructionDefined = true;
+                break;
+
+            case 0b1101001000: // EORI
+                System.out.println("EORI " + rd + ", " + rn + ",  #" + imm);
+                instructionDefined = true;
+                break;
+
+            case 0b1011001000: // ORRI
+                System.out.println("ORRI " + rd + ", " + rn + ",  #" + imm);
+                instructionDefined = true;
+                break;
+
+            case 0b1101000100: // SUBI
+                System.out.println("SUBI " + rd + ", " + rn + ",  #" + imm);
+                instructionDefined = true;
+                break;
+
+            case 0b1111000100: // SUBIS
+                System.out.println("SUBIS " + rd + ", " + rn + ",  #" + imm);
+                instructionDefined = true;
+                break;
+            default:
+                break;
         }
-        return binaryArr;
+        if(instructionDefined) {
+			return;
+		}
+        opcode = instruction >>> 24;
+        addr = (instruction >>> 5) & 0x7FFFFF;
+        Rt = instruction & 0x1F;
+        String rt = correctReg(Rt);
+        switch (opcode) {
+
+            case 0b01010100: // B.COND
+                System.out.println("b.cond " + rt + ", " + addr);
+                instructionDefined = true;
+                break;
+            
+            case 0b10110101: // CBNZ
+                System.out.println("CBZ " + rt + ", " + addr);
+                instructionDefined = true;
+                break;
+            
+            case 0b10110100: // CBZ
+                System.out.println("CBNZ " + rt + ", " + addr);
+                instructionDefined = true;
+                break;
+
+            default:
+			    break;
+        }
+        if(instructionDefined) {
+			return;
+		}
+        opcode = instruction >>> 26;
+        addr = (instruction >>> 6) & 0x3FFFFFFF;
+        switch (opcode) {
+
+            case 0b000101: // B
+                System.out.println("B " + addr);
+                instructionDefined = true;
+                break;
+            
+            case 0b100101: // BL
+                System.out.println("BL" + addr);
+                instructionDefined = true;
+                break;
+
+            default:
+			    break;
+        }
+        if(instructionDefined) {
+			return;
+		}
+        System.out.printf("Unknown opcode: %d%n", opcode);
     }
 
-    private static int bitShift(int decimal, int shiftBits){
-        if (shiftBits >= 0) {
-            return decimal << shiftBits;
-        } else {
-            return decimal >> Math.abs(shiftBits);
+    public static String correctReg(int register) {
+    switch (register) {
+        case 28:
+            return "SP";
+        case 29:
+            return "FP";
+        case 30:
+            return "LR";
+        case 31:
+            return "XZR";
+        default:
+            return "X" + register;
         }
     }
-    public static String findRegister(int memorysize){
-        return "X" + memorysize;
-    }
-    
-
-    private static int rType(OpCode opcode, int[] rawBinary){
-        
-        int rmInt = binaryToDecimal(Arrays.copyOfRange(rawBinary, 11,16));
-        printArray(Arrays.copyOfRange(rawBinary, 12,17));
-        System.out.println();
-        int shamt = binaryToDecimal(Arrays.copyOfRange(rawBinary, 16,22));
-        if(shamt != opcode.getShamt()){
-            throw new RuntimeErrorException(null, "The shamt calculate by OpCode and the one provided in binary do not match");
-        }
-        int rnInt = binaryToDecimal(Arrays.copyOfRange(rawBinary, 22,27));
-        int rdInt = binaryToDecimal(Arrays.copyOfRange(rawBinary, 27,32));
-        
-        String name = opcode.getName();
-        String rm = findRegister(rmInt);
-        String rn = findRegister(rnInt);
-        String rd = findRegister(rdInt);
-        System.out.print(name+ " " + rm + ", " + rn + ", " + rd);
-
-
-        return 0;
-    }
-
-
 }
